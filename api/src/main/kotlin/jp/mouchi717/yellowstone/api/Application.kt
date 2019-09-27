0718e1b67ab3845f4bd63ba13becef674cfe8988
mouchi717 <ouchi.masayuki.0717@gmail.com>
2019-09-27 15:50:48 +0900

    chore: SprintBootの設定をbuild.gradleに追加

---
+++ api/src/main/kotlin/jp/mouchi717/yellowstone/api/Application.kt
@@ -0,0 +1,11 @@
+package jp.mouchi717.yellowstone.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ api/src/main/kotlin/jp/mouchi717/yellowstone/api/controller/HelloController.kt
@@ -0,0 +1,11 @@
+package jp.mouchi717.yellowstone.api.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String = "hello"
+}
